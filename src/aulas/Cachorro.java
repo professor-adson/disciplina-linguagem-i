aa450839c139610e84d388dfbf774248e712e39c
adsonrocha <adsonrocha@gmail.com>
2022-03-07 22:31:39 -0300

    .

---
+++ src/aulas/Cachorro.java
@@ -0,0 +1,14 @@
+package aulas;
+
+public class Cachorro {
+    String raca;
+
+    public void correr() {
+        System.out.println("correu");
+    }
+
+    public static void emitirSom(String som){
+        System.out.println(som);
+    }
+
+}
---
+++ src/aulas/Teste.java
@@ -0,0 +1,15 @@
+package aulas;
+
+public class Teste {
+    public static void main(String[] args) {
+        Cachorro c1 = new Cachorro();
+        Cachorro c2 = new Cachorro();
+        c1.correr();
+        c2.raca = "pitbull";
+        System.out.println(c2.raca);
+
+//        c1.emitirSom("au au");
+//        Cachorro.emitirSom("au au au");
+//        Cachorro.correr();
+    }
+}
