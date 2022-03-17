aaf09b03acd5662f620160d182b7c83fab6779a3
adsonrocha <adsonrocha@gmail.com>
2022-03-16 22:01:47 -0300

    .

---
+++ src/aula5/Teste.java
@@ -0,0 +1,11 @@
+package aula5;
+
+public class Teste {
+    public static void main(String[] args) {
+        Mensagem2 m2 = new Mensagem2();
+        Mensagem2 m3 = new Mensagem2("01/01", "adson", "");
+
+        System.out.println(m2.autor);
+        System.out.println(m3.autor);
+    }
+}
