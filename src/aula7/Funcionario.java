0a86dca68436736c911e21c0a3bcfac719a3fb5c
adsonrocha <adsonrocha@gmail.com>
2022-03-28 22:30:11 -0300

    .

---
+++ src/aula7/Funcionario.java
@@ -0,0 +1,37 @@
+package aula7;
+
+public class Funcionario {
+    protected String nome;
+    protected String matricula;
+    protected double salarioBase;
+
+    public Funcionario(String nome, String matricula, double salarioBase) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+}
---
+++ src/aula7/Gerente.java
@@ -0,0 +1,7 @@
+package aula7;
+
+public class Gerente extends Funcionario {
+    public Gerente(String nome, String matricula, double salarioBase) {
+        super(nome, matricula, salarioBase);
+    }
+}
