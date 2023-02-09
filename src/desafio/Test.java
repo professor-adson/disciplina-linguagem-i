2d8c9377d82464e9dba9217021133da382b98c88
adsonrocha <adsonrocha@gmail.com>
2023-02-09 20:35:54 -0300

    .

---
+++ src/desafio/Test.java
@@ -0,0 +1,14 @@
+package desafio;
+
+import java.util.Scanner;
+
+public class Test {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("Digite S ou N:");
+    char b = s.next().charAt(0);
+
+    System.out.println(b);
+  }
+}
